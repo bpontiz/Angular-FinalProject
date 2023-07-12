c9b44ee965fa50db3ddd147f38713ccb9625809a
Bruno Pontiz <bpontiz@gmail.com>
2023-07-12 11:52:41 -0300

    Final project has been initialized. First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentsAbmComponent } from './students-abm/students-abm.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentsComponent,
+    StudentsAbmComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/students-abm/students-abm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-abm',
+  templateUrl: './students-abm.component.html',
+  styleUrls: ['./students-abm.component.scss']
+})
+export class StudentsAbmComponent {
+
+}
