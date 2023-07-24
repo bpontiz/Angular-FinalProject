b13ed2fb24b6594946cdef7b3f46fc3f8294bcc4
Bruno Pontiz <bpontiz@gmail.com>
2023-07-24 17:39:33 -0300

    RxJS has been implemented for the first time on Student Service.

--- src/app/students-abm/students-abm.component.ts
+++ src/app/students-abm/students-abm.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { Student } from '../students/model/student.model';
 import { StudentsAbmService } from './students-abm.service';
+import { Observable, Subject } from 'rxjs';
 
 
 interface StudentModel {
@@ -17,12 +18,20 @@ interface StudentModel {
   templateUrl: './students-abm.component.html',
   styleUrls: ['./students-abm.component.scss']
 })
-export class StudentsAbmComponent {
+export class StudentsAbmComponent implements OnDestroy {
   constructor(private fb: FormBuilder, private studentService: StudentsAbmService) {
-    this.students = this.studentService.getStudents();
-  }
+    this.studentService.loadStudents();
 
-  public students: Student[] = [];
+    this.students = this.studentService.getStudents();
+  };
+
+  ngOnDestroy(): void {
+    this.destroyed.next(true);
+  };
+
+  public students: Observable<Student[]>;
+
+  public destroyed = new Subject<boolean>();
 
   validateRequired = Validators.required;
 
@@ -59,88 +68,22 @@ export class StudentsAbmComponent {
   };
 
   onSubmit(): void {
-    this.students = [
-      ...this.students,
+    this.studentService.addStudent(
       {
-        id: this.students.length + 1,
+        id: 1,
         name: this.student_name_control.value,
         surname: this.student_surname_control.value,
         email: this.email.value,
         course: this.course.value
       }
-    ]
-  };
+    );
+  }
 
   onDeleteStudent(deleteStudent: Student): void {
-    const confirmAction: boolean = confirm(
-      `Are you sure you want to delete student ${deleteStudent.name} ${deleteStudent.surname} ?`);
-
-    if (confirmAction) {
-      this.students = this.students.filter( element => element.id !== deleteStudent.id);
-    }
+    this.studentService.deleteStudent(deleteStudent);
   };
 
   onEditStudent(editStudent: Student): void {    
-    const editAction = prompt(
-      `Choose one to update:\n(1) Name\n(2) Surname\n(3) Email\n(4) Course\n(5) Press any key to cancel`
-    );
-
-    switch (editAction) {
-      case '1':
-        const editName: string | null = prompt('New student name: ');
-
-        if (editName) {
-          editStudent.name = editName;
-
-          this.students = this.students.map( el => ({
-            ...el,
-            editStudent
-          }));
-        }
-        
-        break;
-
-      case '2':
-        const editSurname: string | null = prompt('New student surname: ');
-
-        if (editSurname) {
-          editStudent.surname = editSurname;
-
-          this.students = this.students.map( el => ({
-            ...el,
-            editStudent
-          }));
-        }
-        
-        break;
-
-      case '3':
-        const editEmail: string | null = prompt('New student email: ');
-
-        if (editEmail) {
-          editStudent.email = editEmail;
-
-          this.students = this.students.map( el => ({
-            ...el,
-            editStudent
-          }));
-        }
-        
-        break;
-
-      case '4':
-        const editCourse: string | null = prompt('New student course: ');
-
-        if (editCourse) {
-          editStudent.course = editCourse;
-
-          this.students = this.students.map( el => ({
-            ...el,
-            editStudent
-          }));
-        }
-        
-        break;
-    }
-  };
+    this.studentService.editStudent(editStudent);
+  }
 }
--- src/app/students-abm/students-abm.service.ts
+++ src/app/students-abm/students-abm.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Student } from '../students/model/student.model';
+import { BehaviorSubject, Observable, Subject, delay, of, take } from 'rxjs';
+
+const students_db: Observable<Student[]> = of([
+  {id: 1, name: 'PAT', surname: 'MCMILLAN', email: 'pat@example.com', course: 'Angular'},
+]).pipe(delay(1000));
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +12,126 @@ import { Student } from '../students/model/student.model';
 export class StudentsAbmService {
   constructor() { }
 
-  private students = [];
+  private _students$ = new BehaviorSubject<Student[]>([]);
 
-  getStudents(): Student[] {
-    return this.students;
+  private students$ = this._students$.asObservable();
+
+  loadStudents(): void {
+    students_db.subscribe({
+      next: (studentsFromDb) => this._students$.next(studentsFromDb)
+    });    
+  };
+
+  getStudents(): Observable<Student[]> {
+    return this.students$;
+  };
+
+  addStudent(newStudent: Student): void {
+    this.students$.pipe(take(1)).subscribe({
+      next: (oldCollection) => {
+        this._students$.next([
+          ...oldCollection,
+          {
+            ...newStudent,
+            id: oldCollection.length + 1
+          }
+        ]);
+      }
+    })
+  }
+
+  deleteStudent(studentToDelete: Student): void {
+    const confirmAction: boolean = confirm(
+      `Are you sure you want to delete student ${studentToDelete.name?.toUpperCase()} ${studentToDelete.surname?.toUpperCase()} ?`);
+
+    if (confirmAction) {
+      this.students$.pipe(take(1)).subscribe({
+        next: (oldCollection) => {
+          this._students$.next(
+            oldCollection.filter( item => item.id !== studentToDelete.id)
+          )
+        }
+      })
+    };
+  };
+
+  editStudent(studentToEdit: Student): void {
+    this.students$.pipe(take(1)).subscribe({
+      next: (oldCollection) => {
+        const editAction = prompt(
+          `--------------\nEditing ${studentToEdit.name?.toUpperCase()} ${studentToEdit.surname?.toUpperCase()}\nChoose one to update:\n(1) Name\n(2) Surname\n(3) Email\n(4) Course\n(5) Press any key to cancel`
+        );
+        switch (editAction) {
+          case '1':
+            const editName: string | null = prompt(`--------------\nEditing ${studentToEdit.name?.toUpperCase()} ${studentToEdit.surname?.toUpperCase()}\nNew student name: `);
+
+            if (editName) {
+              studentToEdit.name = editName;
+
+              this._students$.next(
+                oldCollection.map( el => ({
+                  ...el,
+                  studentToEdit
+                }))
+              );
+            }
+            
+            break;
+
+          case '2':
+            const editSurname: string | null = prompt(`--------------\nEditing ${studentToEdit.name?.toUpperCase()} ${studentToEdit.surname?.toUpperCase()}\nNew student surname: `);
+
+            if (editSurname) {
+              studentToEdit.surname = editSurname;
+
+              this._students$.next(
+                oldCollection.map( el => ({
+                  ...el,
+                  studentToEdit
+                }))
+              );
+            }
+            
+            break;
+
+          case '3':
+            const editEmail: string | null = prompt(`--------------\nEditing ${studentToEdit.name?.toUpperCase()} ${studentToEdit.surname?.toUpperCase()}\nNew student email: `);
+
+            if (editEmail) {
+              studentToEdit.email = editEmail;
+
+              this._students$.next(
+                oldCollection.map( el => ({
+                  ...el,
+                  studentToEdit
+                }))
+              );
+            }
+            
+            break;
+
+          case '4':
+            const editCourse: string | null = prompt(`--------------\nEditing ${studentToEdit.name?.toUpperCase()} ${studentToEdit.surname?.toUpperCase()}\nNew student course: `);
+
+            if (editCourse) {
+              studentToEdit.course = editCourse;
+
+              this._students$.next(
+                oldCollection.map( el => ({
+                  ...el,
+                  studentToEdit
+                }))
+              );
+            }
+            
+            break;
+
+            default:
+              return this.students$;
+        };
+
+        return this.students$;
+      }
+    });
   }
 }
