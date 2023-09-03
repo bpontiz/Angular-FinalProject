6f4dfb36425997383fce5d114941bee2c89ae575
bpontiz <bpontiz@gmail.com>
2023-09-03 16:05:56 -0300

    Ngrx effects has been implemented on Enrollment-Abm component.

--- src/app/store/enrollment-abm/enrollment-abm.effects.ts
+++ src/app/store/enrollment-abm/enrollment-abm.effects.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 
-import { concatMap } from 'rxjs/operators';
-import { Observable, EMPTY } from 'rxjs';
+import { catchError, concatMap, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { EnrollmentAbmActions } from './enrollment-abm.actions';
+import { HttpClient } from '@angular/common/http';
+import { Enrollment } from 'src/app/dashboard/enrollment/model/enrollment.model';
 
 @Injectable()
 export class EnrollmentAbmEffects {
-
+  constructor(private actions$: Actions, private httpClient: HttpClient) {}
 
   loadEnrollmentAbms$ = createEffect(() => {
     return this.actions$.pipe(
-
+      
       ofType(EnrollmentAbmActions.loadEnrollmentAbms),
       /** An EMPTY observable only emits completion. Replace with your own observable API request */
-      concatMap(() => EMPTY as Observable<{ type: string }>)
-    );
-  });
-
-  constructor(private actions$: Actions) {}
+      concatMap(() => this.getEnrollmentsFromDB().pipe(
+        map(() => EnrollmentAbmActions.loadEnrollmentAbms()),
+        catchError(() => of(EnrollmentAbmActions.loadEnrollmentAbms()))
+      ))
+      );
+    });
+    
+    
+    private getEnrollmentsFromDB(): Observable<Enrollment> {
+      return this.httpClient.get<Enrollment>('http://localhost:3000/enrollments');
+  };
 }
